45447e5973e3266454354268882f2d358574fbcf
metal0l <metal0l@outlook.com>
2020-05-30 16:56:01 +0500

    Build

---
+++ Calcelerrere/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using Calcelerrere.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calcelerrere.Controllers
+{
+    public class CalculatorController : Controller
+    {
+
+        // GET: Calculator/
+        public ActionResult Index()
+        {
+            return View(new Calculate());
+        }
+        [HttpPost]
+        public ActionResult Index(Calculate c,int FN,int SN, string calculate)
+        {
+            int total = 0;
+            if (calculate == "add")
+            {
+                total = FN + SN;
+            }
+            else if (calculate == "sub")
+            {
+              //  c.result = c.summ - c.time;
+            }
+            else if (calculate == "mul")
+            {
+              //  c.result = c.summ * c.time;
+            }
+            else
+            {
+               // c.result = c.summ / c.time;
+            }
+            c.result = total;
+            
+            return View(c);
+        }
+    }
+}
---
+++ Calcelerrere/Models/Calculate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calcelerrere.Models
+{
+    public class Calculate
+    {
+        public int summ { get; set; }
+
+        public int time { get; set; }
+
+        public int percent { get; set; }
+
+        public int result { get; set; }
+    }
+}
